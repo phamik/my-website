e7d9b2e436297af9fd0af42573d9a64a1005aa50
phamik <tien.pham@seznam.cz>
2018-06-18 01:07:28 +0200

    animated text running

---
+++ animatedString.js
@@ -0,0 +1,106 @@
+
+    var Messenger = function(el, word){
+    'use strict';
+    var m = this;
+    
+    m.init = function(){
+      m.codeletters = "&#*+%?£@§$";
+      m.message = 0;
+      m.current_length = 0;
+      m.fadeBuffer = false;
+      m.messages = [
+        word
+        // 'Hi,',
+        // 'My name is Peter.',
+        // 'And I am a developer'
+      ];
+      
+      setTimeout(m.animateIn, 100);
+    };
+    
+    m.generateRandomString = function(length){
+      var random_text = '';
+      while(random_text.length < length){
+        random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+      } 
+      
+      return random_text;
+    };
+    
+    m.animateIn = function(){
+      if(m.current_length < m.messages[m.message].length){
+        m.current_length = m.current_length + 2;
+        if(m.current_length > m.messages[m.message].length) {
+          m.current_length = m.messages[m.message].length;
+        }
+        
+        var message = m.generateRandomString(m.current_length);
+        $(el).html(message);
+        
+        setTimeout(m.animateIn, 20);
+      } else { 
+        setTimeout(m.animateFadeBuffer, 20);
+      }
+    };
+    
+    m.animateFadeBuffer = function(){
+      if(m.fadeBuffer === false){
+        m.fadeBuffer = [];
+        for(var i = 0; i < m.messages[m.message].length; i++){
+          m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
+        }
+      }
+      
+      var do_cycles = false;
+      var message = ''; 
+      
+      for(var i = 0; i < m.fadeBuffer.length; i++){
+        var fader = m.fadeBuffer[i];
+        if(fader.c > 0){
+          do_cycles = true;
+          fader.c--;
+          message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+        } else {
+          message += fader.l;
+        }
+      }
+      
+      $(el).html(message);
+      setTimeout(m.animateFadeBuffer, 50);
+      // if(do_cycles === true){
+      //   setTimeout(m.animateFadeBuffer, 50);
+      // } else {
+      //   // setTimeout(m.cycleText, 2000);
+      // }
+    };
+    
+    // m.cycleText = function(){
+    //   m.message = m.message + 1;
+    //   if(m.message >= m.messages.length){
+    //     m.message = 0;
+    //   }
+      
+    //   m.current_length = 0;
+    //   m.fadeBuffer = false;
+    //   $(el).html('');
+      
+    //   setTimeout(m.animateIn, 200);
+    // };
+    
+    m.init();
+  }
+  
+  console.clear();
+  var messenger = new Messenger($('#messenger'), 'Hi,');
+  // var messenger1 = new Messenger($('#messenger1'), 'My name is Tien');
+  // var messenger2 = new Messenger($('#messenger2'), 'and I am a developer');
+
+  setTimeout(function first(){
+    var messenger1 = new Messenger($('#messenger1'), 'My name is Tien');
+  }, 1000);
+
+  setTimeout(function first(){
+    var messenger2 = new Messenger($('#messenger2'), 'and I am a developer');
+  }, 2000);
+
+
