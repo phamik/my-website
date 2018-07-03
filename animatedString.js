98e87781d11a43fbca27886963bac7ef68ba68f8
phamik <tien.pham@seznam.cz>
2018-07-03 18:20:58 +0100

    progress so far

--- animatedString.js
+++ animatedString.js
@@ -96,11 +96,12 @@
   // var messenger2 = new Messenger($('#messenger2'), 'and I am a developer');
 
   setTimeout(function first(){
-    var messenger1 = new Messenger($('#messenger1'), 'My name is Tien,');
+    var messenger1 = new Messenger($('#messenger1'), 'My name is Tien');
   }, 1000);
 
   setTimeout(function first(){
     var messenger2 = new Messenger($('#messenger2'), 'and I am a developer');
   }, 2000);
 
+  
 
